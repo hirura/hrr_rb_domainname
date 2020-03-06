8e14fec99155672987ba28dbc274edd1a3b8e863
hirura <hirura@gmail.com>
2020-03-06 20:47:35 +0900

    update spec file

--- spec/hrr_rb_domainname_spec.rb
+++ spec/hrr_rb_domainname_spec.rb
@@ -3,7 +3,49 @@ RSpec.describe HrrRbDomainname do
     expect(HrrRbDomainname::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".get" do
+    it "returns domainname" do
+      expect(HrrRbDomainname.get).to eq `domainname`.chomp
+    end
+  end
+
+  describe ".getdomainname" do
+    it "returns domainname" do
+      expect(HrrRbDomainname.getdomainname).to eq `domainname`.chomp
+    end
+  end
+
+  describe ".set" do
+    before :example do
+      @name_bak = `domainname`.chomp
+    end
+
+    after :example do
+      `domainname "#{@name_bak}"`
+    end
+
+    let(:name){ "new#{@name_bak}" }
+
+    it "sets domainname and returns it" do
+      expect(HrrRbDomainname.set(name)).to eq name
+      expect(`domainname`.chomp).to eq name
+    end
+  end
+
+  describe ".setdomainname" do
+    before :example do
+      @name_bak = `domainname`.chomp
+    end
+
+    after :example do
+      `domainname "#{@name_bak}"`
+    end
+
+    let(:name){ "new#{@name_bak}" }
+
+    it "sets domainname and returns it" do
+      expect(HrrRbDomainname.setdomainname(name)).to eq name
+      expect(`domainname`.chomp).to eq name
+    end
   end
 end
