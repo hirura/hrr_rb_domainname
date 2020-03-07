2c5fc1313b881f21d6f5b059a308d3d7be91727c
hirura <hirura@gmail.com>
2020-03-07 18:04:35 +0900

    update with adding documentation

--- ext/hrr_rb_domainname/hrr_rb_domainname.c
+++ ext/hrr_rb_domainname/hrr_rb_domainname.c
@@ -4,6 +4,12 @@
 
 VALUE rb_mHrrRbDomainname;
 
+/*
+ * A wrapper around getdomainname. Returns domainname that is set on the machine.
+ *
+ * @raise [Errno::EINVAL] when the name is too long.
+ * @return [String] domainname that is set on the machine.
+ */
 VALUE
 hrr_rb_domainname_getdomainname(void)
 {
@@ -17,14 +23,21 @@ hrr_rb_domainname_getdomainname(void)
   return rb_str_new_cstr(name);
 }
 
+/*
+ * A wrapper around setdomainname. Returns domainname that is updated on the machine.
+ *
+ * @param [String] name domainname to be set
+ * @raise [Errno::EFAULT] when the name pointed outside of user address space.
+ * @raise [Errno::EINVAL] when the name is too long.
+ * @raise [Errno::EPERM ] when the caller did not have the CAP_SYS_ADMIN capability in the user namespace associated with its UTS namespace (see namespaces(7)).
+ * @return [String] domainname that is updated on the machine
+ */
 VALUE
-hrr_rb_domainname_setdomainname(VALUE self, VALUE arg)
+hrr_rb_domainname_setdomainname(VALUE self, VALUE name)
 {
-  char *name;
+  char *dname = StringValueCStr(name);
 
-  name = StringValueCStr(arg);
-
-  if (setdomainname(name, strlen(name)) < 0)
+  if (setdomainname(dname, strlen(dname)) < 0)
     rb_sys_fail("setdomainname");
 
   return hrr_rb_domainname_getdomainname();
@@ -38,5 +51,4 @@ Init_hrr_rb_domainname(void)
   rb_define_singleton_method(rb_mHrrRbDomainname, "getdomainname", hrr_rb_domainname_getdomainname, 0);
   rb_define_singleton_method(rb_mHrrRbDomainname, "set",           hrr_rb_domainname_setdomainname, 1);
   rb_define_singleton_method(rb_mHrrRbDomainname, "setdomainname", hrr_rb_domainname_setdomainname, 1);
-
 }
